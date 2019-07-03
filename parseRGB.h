91b451c33b6ab11a89b8e6634c0c56e74c1e057c
yumetodo <yume-wikijp@live.jp>
2019-07-03 13:44:36 +0900

    feat: create header

---
+++ parseRGB.h
@@ -0,0 +1,12 @@
+﻿#ifndef PARSERGB_73954723913952734592_H_
+#define PARSERGB_73954723913952734592_H_
+#include <stdbool.h>
+#include <stdint.h>
+#ifdef __cplusplus
+extern "C" {
+#endif //__cplusplus
+bool parseRGB(char *srcPtr, uint8_t *Red, uint8_t *Green, uint8_t *Blue);
+#ifdef __cplusplus
+}
+#endif //__cplusplus
+#endif //PARSERGB_73954723913952734592_H_
